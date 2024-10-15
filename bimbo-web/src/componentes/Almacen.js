82f54c3d62949b3822c5dd9fffadf3681f91db8a
A01753505 <A01753505@tec.mx>
2024-10-14 19:53:39 -0600

    Semaforo funcional

--- bimbo-web/src/componentes/Almacen.js
+++ bimbo-web/src/componentes/Almacen.js
@@ -2,36 +2,31 @@ import "../styles/App.css";
 import Porcentaje from "./Porcentaje";
 
 const Almacen = () => {
+  const values1 = [7, 8, 5, 0, 8, 8];
+  const values2 = [8, 6, 8, 2, 6, 7];
+  const values3 = [0, 3, 8, 4, 8, 1];
+
   return (
     <div className="almacen">
-        <div className="columna">
-            <Porcentaje />
-            <Porcentaje />
-            <Porcentaje />
-            <Porcentaje />
-            <Porcentaje />
-            <Porcentaje />
-        </div>
-        <div className="columna">
-            <Porcentaje />
-            <Porcentaje />
-            <Porcentaje />
-            <Porcentaje />
-            <Porcentaje />
-            <Porcentaje />
-        </div>
-        <div className="columna">
-            <Porcentaje />
-            <Porcentaje />
-            <Porcentaje />
-            <Porcentaje />
-            <Porcentaje />
-            <Porcentaje />
-        </div>
-        <div className="columna">
-            <div className="oficina"/>
-            <div className="oficina"/>
-        </div>
+      <div className="columna">
+        {values1.map((value, index) => (
+          <Porcentaje key={index} value={value} />
+        ))}
+      </div>
+      <div className="columna">
+        {values2.map((value, index) => (
+          <Porcentaje key={index} value={value} />
+        ))}
+      </div>
+      <div className="columna">
+        {values3.map((value, index) => (
+          <Porcentaje key={index} value={value} />
+        ))}
+      </div>
+      <div className="columna">
+        <div className="oficina" />
+        <div className="oficina" />
+      </div>
     </div>
   );
 };
--- bimbo-web/src/componentes/Porcentaje.js
+++ bimbo-web/src/componentes/Porcentaje.js
@@ -1,17 +1,32 @@
 import "../styles/App.css";
 
-const Porcentaje = () => {
+const Porcentaje = ({value}) => {
+  const bars = [];
+
+  if (value >= 7) {
+    for (let i = 0; i < value; i++) {
+      bars.push(<div key={i+"V"} className="barra-verde"/>);
+    }
+  } else if (value >= 4) {
+    for (let i = 0; i < value; i++) {
+      bars.push(<div key={i+"A"} className="barra-amarilla"/>);
+    }
+  } else {
+    for (let i = 0; i < value; i++) {
+      bars.push(<div key={i+"R"} className="barra-roja"/>);
+    }
+  }
+
+  if (value < 8) {
+    for (let i = 0; i < 8-value; i++) {
+      bars.push(<div key={i+"G"} className="barra-gris"/>);
+    }
+  }
+
   return (
     <div className="cuadrado">
       <div className="circulo">
-        <div className="barra-verde"/>
-        <div className="barra-amarilla"/>
-        <div className="barra-roja"/>
-        <div className="barra-gris"/>
-        <div className="barra-gris"/>
-        <div className="barra-gris"/>
-        <div className="barra-gris"/>
-        <div className="barra-gris"/>
+        {bars}
       </div>
     </div>
   );
