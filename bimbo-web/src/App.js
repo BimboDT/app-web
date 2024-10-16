0e112f3fb1563b59c91c97fd7dc8a359b8ee58d7
Zurita02 <dgzurivl@gmail.com>
2024-10-15 21:33:24 -0600

    Rack (Estantes y columnas)

--- bimbo-web/src/App.js
+++ bimbo-web/src/App.js
@@ -1,12 +1,13 @@
 import './styles/App.css';
 import Almacen from './componentes/Almacen';
+import Rack from './componentes/Rack';
 
 function App() {
   return (
     <div className="App">
       <br/>
       <br/>
-      <Almacen/>
+      <Rack/>
     </div>
   );
 }
---
+++ bimbo-web/src/componentes/Rack.js
@@ -0,0 +1,24 @@
+import "../styles/App.css";
+
+
+const Rack = () => {
+    return (
+        <div className='rack'>
+            <div className= 'background-cols'>
+                <div className='col'></div>
+                <div className='col'></div>
+                <div className='col'></div>
+            </div>
+            <div className='estante'>
+            </div>
+            <div className='estante'>
+            </div>
+            <div className='estante'>
+            </div>
+            <div className='estante'>
+            </div>
+        </div>
+    );
+};
+
+export default Rack;
