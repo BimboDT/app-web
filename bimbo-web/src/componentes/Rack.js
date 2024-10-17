594d359ecf728db039a8507c2f6e7aa1b1394550
Zurita02 <dgzurivl@gmail.com>
2024-10-16 19:05:03 -0600

    Cajas y botón de regresar (Rack)

--- bimbo-web/src/componentes/Rack.js
+++ bimbo-web/src/componentes/Rack.js
@@ -1,9 +1,11 @@
 import "../styles/App.css";
-
+import { IoIosArrowBack } from "react-icons/io";
 
 const Rack = () => {
     return (
         <div className='rack'>
+            <button className='btn-back'><IoIosArrowBack size={34} /></button>
+            <div className='rack'>
             <div className= 'background-cols'>
                 <div className='col'></div>
                 <div className='col'></div>
@@ -11,13 +13,42 @@ const Rack = () => {
             </div>
             <div className='estante'>
             </div>
-            <div className='estante'>
+            <div className = 'cajas'>
+                <div className ='caja'><div className='etiqueta'></div></div>
+                <div className ='caja'><div className='etiqueta'></div></div>
+                <div className ='caja'><div className='etiqueta'></div></div>
+                <div className ='caja'><div className='etiqueta'></div></div>
+                <div className ='caja'><div className='etiqueta'></div></div>
+                <div className ='caja'><div className='etiqueta'></div></div>
             </div>
             <div className='estante'>
             </div>
+            <div className = 'cajas'>
+                <div className ='caja'><div className='etiqueta'></div></div>
+                <div className ='caja'><div className='etiqueta'></div></div>
+                <div className ='caja'><div className='etiqueta'></div></div>
+                <div className ='caja'><div className='etiqueta'></div></div>
+                <div className ='caja'><div className='etiqueta'></div></div>
+                <div className ='caja'><div className='etiqueta'></div></div>
+            </div>
+            <div className='estante'>
+            </div>
+            <div className = 'cajas'>
+                <div className ='caja'><div className='etiqueta'></div></div>
+                <div className ='caja'><div className='etiqueta'></div></div>
+                <div className ='caja'><div className='etiqueta'></div></div>
+                <div className ='caja'><div className='etiqueta'></div></div>
+                <div className ='caja'><div className='etiqueta'></div></div>
+                <div className ='caja'><div className='etiqueta'></div></div>
+            </div>
+            
+            
+            
             <div className='estante'>
             </div>
         </div>
+        </div>
+        
     );
 };
 
