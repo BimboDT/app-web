6335f2517fc27f7807be2fe22e9d115a500816d2
A01753505 <A01753505@tec.mx>
2024-10-16 18:53:19 -0600

    Filtro implementado

--- bimbo-web/src/componentes/Almacen.js
+++ bimbo-web/src/componentes/Almacen.js
@@ -1,27 +1,36 @@
 import "../styles/App.css";
 import Porcentaje from "./Porcentaje";
+import Incidencias from "./Incidencias";
 
-const Almacen = () => {
-  const values1 = [7, 8, 5, 0, 8, 8];
-  const values2 = [8, 6, 8, 2, 6, 7];
-  const values3 = [0, 3, 8, 4, 8, 1];
-
+const Almacen = ({ almacenValues, selectedOption }) => {
   return (
     <div className="almacen">
       <div className="columna">
-        {values1.map((value, index) => (
-          <Porcentaje key={index} value={value} />
-        ))}
+        {almacenValues[0].map((value, index) => {
+          return selectedOption === "Cantidad de incidencias" ? (
+            <Incidencias key={index} value={value} />
+          ) : (
+            <Porcentaje key={index} value={value} />
+          );
+        })}
       </div>
       <div className="columna">
-        {values2.map((value, index) => (
-          <Porcentaje key={index} value={value} />
-        ))}
+        {almacenValues[1].map((value, index) => {
+          return selectedOption === "Cantidad de incidencias" ? (
+            <Incidencias key={index} value={value} />
+          ) : (
+            <Porcentaje key={index} value={value} />
+          );
+        })}
       </div>
       <div className="columna">
-        {values3.map((value, index) => (
-          <Porcentaje key={index} value={value} />
-        ))}
+        {almacenValues[2].map((value, index) => {
+          return selectedOption === "Cantidad de incidencias" ? (
+            <Incidencias key={index} value={value} />
+          ) : (
+            <Porcentaje key={index} value={value} />
+          );
+        })}
       </div>
       <div className="columna">
         <div className="oficina" />
---
+++ bimbo-web/src/componentes/Filtro.js
@@ -0,0 +1,86 @@
+import { useEffect, useState, useRef, forwardRef } from "react";
+import "../styles/Dropdown.css";
+
+const Filtro = ({ updateAlmacenValues, setSelectedOption }) => {
+  const [open, setOpen] = useState(false);
+  const [dropdownTop, setDropdownTop] = useState(0);
+  const [titulo, setTitulo] = useState("Cantidad de producto en racks");
+  const dropdownRef = useRef();
+  const buttonRef = useRef();
+  const contentRef = useRef();
+
+  const toggleDropdown = () => {
+    if (!open) {
+      const spaceRemaining =
+        window.innerHeight - buttonRef.current.getBoundingClientRect().bottom;
+      const contentHeight = contentRef.current.clientHeight;
+
+      const topPosition =
+        spaceRemaining > contentHeight
+          ? null
+          : -(contentHeight - spaceRemaining);
+      setDropdownTop(topPosition);
+    }
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleButtonClick = (text) => {
+    setTitulo(text);
+    updateAlmacenValues(text);
+    setSelectedOption(text);
+    setOpen(false);
+  };
+
+  useEffect(() => {
+    const handler = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setOpen(false);
+      }
+    };
+
+    document.addEventListener("click", handler);
+    return () => {
+      document.removeEventListener("click", handler);
+    };
+  }, []);
+
+  return (
+    <div ref={dropdownRef} className="dropdown">
+      <DropdownButton ref={buttonRef} toggle={toggleDropdown} open={open} titulo={titulo}/>
+      <DropdownContent ref={contentRef} top={dropdownTop} open={open}>
+        <button className="dropdown-item" onClick={() => handleButtonClick("Porcentaje de conteo realizado")}>Porcentaje de conteo realizado</button>
+        <button className="dropdown-item" onClick={() => handleButtonClick("Cantidad de incidencias")}>Cantidad de incidencias</button>
+        <button className="dropdown-item" onClick={() => handleButtonClick("Cantidad de producto en racks")}>Cantidad de producto en racks</button>
+      </DropdownContent>
+    </div>
+  );
+};
+
+const DropdownButton = forwardRef((props, ref) => {
+  const { toggle, open, titulo } = props;
+  return (
+    <div
+      onClick={toggle}
+      className={`dropdown-btn ${open ? "button-open" : ""}`}
+      ref={ref}
+    >
+      <div className="titulo"> {titulo} </div>
+      <span className="toggle-icon">{open ? "▲" : "▼"}</span>
+    </div>
+  );
+});
+
+const DropdownContent = forwardRef((props, ref) => {
+  const { open, top, children } = props;
+  return (
+    <div
+      className={`dropdown-content ${open ? "content-open" : ""}`}
+      style={{ top: top ? `${top}px` : "100%" }}
+      ref={ref}
+    >
+      {children}
+    </div>
+  );
+});
+
+export default Filtro;
