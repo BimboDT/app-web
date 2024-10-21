2987d63142ce2febb46a1fd1d14a2195092c838f
A01753505 <A01753505@tec.mx>
2024-10-21 16:31:45 -0600

    Cambio de almacen a racks

--- bimbo-web/src/App.js
+++ bimbo-web/src/App.js
@@ -1,15 +1,15 @@
 import "./styles/App.css";
 import { useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Almacen from "./componentes/Almacen";
 import Filtro from "./componentes/Filtro";
-import Rack from './componentes/Rack';
+import Rack from "./componentes/Rack";
 
 function App() {
   const [almacenValues, setAlmacenValues] = useState([
     [6, 4, 6, 8, 1, 1],
     [3, 8, 7, 8, 7, 5],
-    [3, 4, 0, 8, 2, 2]
+    [3, 4, 0, 8, 2, 2],
   ]);
 
   const [selectedOption, setSelectedOption] = useState(
@@ -22,10 +22,10 @@ function App() {
         setAlmacenValues([
           [6, 4, 6, 8, 1, 1],
           [3, 8, 7, 8, 7, 5],
-          [3, 4, 0, 8, 2, 2]
+          [3, 4, 0, 8, 2, 2],
         ]);
         break;
-        case "Cantidad de incidencias":
+      case "Cantidad de incidencias":
         setAlmacenValues([
           [2, 3, 1, 0, 5, 2],
           [4, 1, 2, 6, 3, 5],
@@ -36,7 +36,7 @@ function App() {
         setAlmacenValues([
           [7, 8, 5, 0, 8, 8],
           [8, 6, 8, 2, 6, 7],
-          [0, 3, 8, 4, 8, 1]
+          [0, 3, 8, 4, 8, 1],
         ]);
         break;
       default:
@@ -52,15 +52,18 @@ function App() {
             path="/almacen"
             element={
               <>
-                <Filtro updateAlmacenValues={updateAlmacenValues} setSelectedOption={setSelectedOption} />
-                <Almacen almacenValues={almacenValues} selectedOption={selectedOption} />
+                <Filtro
+                  updateAlmacenValues={updateAlmacenValues}
+                  setSelectedOption={setSelectedOption}
+                />
+                <Almacen
+                  almacenValues={almacenValues}
+                  selectedOption={selectedOption}
+                />
               </>
             }
           />
-          <Route
-            path="/rack"
-            element={<Rack />}
-          />
+          <Route path="/rack" element={<Rack />} />
         </Routes>
       </div>
     </Router>
