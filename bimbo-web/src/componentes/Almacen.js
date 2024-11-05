1c34e43fddfec9e77586970bfc087f2951c67b55
A01753505 <A01753505@tec.mx>
2024-11-04 19:14:18 -0600

    Actualización en página web

--- bimbo-web/src/componentes/Almacen.js
+++ bimbo-web/src/componentes/Almacen.js
@@ -3,7 +3,7 @@ import Porcentaje from "./Porcentaje";
 import Incidencias from "./Incidencias";
 import { Link } from "react-router-dom";
 
-const Almacen = ({ almacenValues, selectedOption, setIsHovered, updateLocation }) => {
+const Almacen = ({ almacenValues, selectedOption, setIsHovered, locations, info, setSelectedRack }) => {
   const Componente =
     selectedOption === "Cantidad de incidencias" ? Incidencias : Porcentaje;
 
@@ -11,11 +11,34 @@ const Almacen = ({ almacenValues, selectedOption, setIsHovered, updateLocation }
     <div className="almacen">
       {almacenValues.map((values, colIndex) => (
         <div className="columna" key={colIndex}>
-          {values.map((value, index) => (
-            <Link to="/rack" key={index} onMouseEnter={() => updateLocation(`${colIndex}-${index}`)}>
-              <Componente value={value} setIsHovered={setIsHovered} />
-            </Link>
-          ))}
+          {values.map((value, index) => {
+            const location = locations[colIndex][index];
+            let extra = '';
+
+            if (selectedOption === "Cantidad de producto en racks") {
+              extra = " cajas almacenadas";
+            } else if (selectedOption === "Cantidad de incidencias") {
+              extra = " incidencias reportadas";
+            } else {
+              extra = " % de conteo realizado";
+            }
+
+            return (
+              <div style={{width: "100%", maxWidth: "200px", margin: "5px"}} key={`${colIndex}-${index}`}>
+                <Link
+                  to="/rack"
+                  onMouseEnter={() => setSelectedRack(info[colIndex][index] + extra)}
+                  onMouseLeave={() => setIsHovered(false)
+                  }
+                >
+                  <Componente value={value} setIsHovered={setIsHovered} />
+                </Link>
+                <div className="ubicacion">
+                  Ubicación: {location}
+                </div>
+              </div>
+            );
+          })}
         </div>
       ))}
     </div>
